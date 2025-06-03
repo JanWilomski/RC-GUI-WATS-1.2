4c411f81a1747fd8ad51705eb240047e6fd01cfb
janwil <jwilomski1@gmail.com>
2025-06-03 10:50:16 +0200

    order book add

--- RC_GUI_WATS/Views/MessagesTabControl.xaml.cs
+++ RC_GUI_WATS/Views/MessagesTabControl.xaml.cs
@@ -1,5 +1,7 @@
-﻿// Views/MessagesTabControl.xaml.cs - Fixed version
+﻿// Views/MessagesTabControl.xaml.cs - Poprawiona wersja
 using System.Windows.Controls;
+using System.Windows.Input;
+using RC_GUI_WATS.Models;
 
 namespace RC_GUI_WATS.Views
 {
@@ -10,9 +12,31 @@ namespace RC_GUI_WATS.Views
             InitializeComponent();
         }
         
+        private void PositionsDataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+        
         private void CcgDataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
         {
             e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+        
+        private void OrderBookDataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+        
+        private void CcgDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            // Get the clicked item
+            if (sender is DataGrid dataGrid && dataGrid.SelectedItem is CcgMessage selectedMessage)
+            {
+                // Open details window
+                var detailsWindow = new CcgMessageDetailsWindow(selectedMessage);
+                detailsWindow.Owner = System.Windows.Application.Current.MainWindow;
+                detailsWindow.ShowDialog();
+            }
         }
     }
 }
